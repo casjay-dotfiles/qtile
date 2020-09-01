281a2549dca40c0935ad8c65c532a5297297572b
casjay <git-admin@casjaysdev.com>
2020-09-01 00:45:38 -0400

    Update compton-toggle.sh

--- scripts/compton-toggle.sh
+++ scripts/compton-toggle.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
-if pgrep -x "compton" > /dev/null
+
+elif pgrep -x "picom" > /dev/null
 then
-	killall compton
+  killall picom
 else
-	compton -b --config $DESKTOP_SESSION_CONFDIR/compton.conf
+  [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
+  [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
+  picom -b
+fi
+
+elif pgrep -x "compton" > /dev/null
+then
+  killall compton
+else
+  [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
+  [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
+  compton -b
 fi
