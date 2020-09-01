e8c763222db4c9e21f211698e6edfccc9748f378
casjay <git-admin@casjaysdev.com>
2020-09-01 00:55:52 -0400

    Update compton-toggle.sh

--- bin/compton-toggle.sh
+++ bin/compton-toggle.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-elif pgrep -x "picom" > /dev/null
-then
-  killall picom
-else
-  [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
-  [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
-  picom -b
-fi
+if [ $(command -v picom 2>/dev/null) ]; then
+  if pgrep -x "picom" >/dev/null; then
+    killall picom
+  else
+    [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
+      [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && picom -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
+      picom -b
+  fi
 
-elif pgrep -x "compton" > /dev/null
-then
-  killall compton
-else
-  [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
-  [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
-  compton -b
+elif [ $(command -v compton 2>/dev/null) ]; then
+  if pgrep -x "compton" >/dev/null; then
+    killall compton
+  else
+    [ -f "$DESKTOP_SESSION_CONFDIR/picom.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/picom.conf ||
+      [ -f "$DESKTOP_SESSION_CONFDIR/compton.conf" ] && compton -b --config $DESKTOP_SESSION_CONFDIR/compton.conf ||
+      compton -b
+  fi
 fi
